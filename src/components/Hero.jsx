4d66d3b27f4f2d1b4c5fcbebe5f74042f936142e
dankukennedy <dankukennedy@gmail.com>
2024-11-17 23:12:18 +0000

    corrections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
             I specialize in building modern and responsive web applications.
          </p>
          <div className='mt-8 space-x-4 '>
-            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
+            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact Me</button>
           <a href={cv} download className="inline-block">
             <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
              My Resume
