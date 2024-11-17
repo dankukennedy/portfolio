26a5e7942dcfbc69b4fdd9d2c3d730b933c7ec08
dankukennedy <dankukennedy@gmail.com>
2024-11-17 21:30:08 +0000

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState } from 'react';
+ import logo from  '../assets/logo.jpeg'
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,8 +12,16 @@ const Navbar = () => {
   return (
     <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
       <div className="container py-2 flex justify-between items-center">
+
         {/* Logo */}
-        <div className="text-2xl font-bold">Edem</div>
+        <a href="#home" className="flex items-center">
+          <img 
+            src={logo} 
+            alt="Logo" 
+            className="w-15 h-10 mr-2 rounded" // Adjust size as needed
+          />
+          <span className="text-2xl font-bold">Edem</span>
+        </a>
         
         {/* Hamburger Icon */}
         <button
@@ -27,7 +36,7 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-6">
           <a href="#home" className="hover:text-gray-400">Home</a>
-          <a href="#about" className="hover:text-gray-400">About Me</a>
+          <a href="#about" className="hover:text-gray-400">About</a>
           <a href="#service" className="hover:text-gray-400">Services</a>
           <a href="#project" className="hover:text-gray-400">Projects</a>
           <a href="#contact" className="hover:text-gray-400">Contact</a>
