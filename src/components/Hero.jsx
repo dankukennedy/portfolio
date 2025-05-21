23ee2e790b098157b6e954d3a623ac57d142dacd
dankukennedy <dankukennedy@gmail.com>
2025-05-21 15:00:44 +0000

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import HeroImage from '../assets/office.jpeg';
+import HeroImage from '../assets/profile.jpeg';
 import cv from '../assets/files/DKE-CV.pdf';
 
 
