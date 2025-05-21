0e22d863be9670d541b19f0cffae9c572763713b
dankukennedy <dankukennedy@gmail.com>
2025-05-21 13:45:20 +0000

    git commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,17 +10,17 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
-      <div className="container py-2 flex justify-between items-center">
+    <nav className="px-8 text-white bg-black md:px-16 lg:px-24">
+      <div className="container flex items-center justify-between py-2">
 
         {/* Logo */}
         <a href="#home" className="flex items-center">
           <img 
             src={logo} 
             alt="Logo" 
-            className="w-15 h-10 mr-2 rounded " // Adjust size as needed
+            className="h-10 mr-2 rounded w-15 " // Adjust size as needed
           />
-          <span className="p-1 text-2xl font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Edem</span>
+          <span className="p-1 text-2xl font-bold rounded hover:bg-gradient-to-r from-green-300 to-blue-500">Edem</span>
         </a>
         
         {/* Hamburger Icon */}
@@ -28,22 +28,22 @@ const Navbar = () => {
           onClick={toggleMenu}
           className="md:hidden focus:outline-none"
         >
-          <span className="block w-8 h-1 bg-white mb-1"></span>
-          <span className="block w-8 h-1 bg-white mb-1"></span>
+          <span className="block w-8 h-1 mb-1 bg-white"></span>
+          <span className="block w-8 h-1 mb-1 bg-white"></span>
           <span className="block w-8 h-1 bg-white"></span>
         </button>
 
         {/* Desktop Menu */}
-        <div className="hidden md:flex space-x-6">
-          <a href="#home"className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Home</a>
-          <a href="#about" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">About</a>
-          <a href="#service" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Services</a>
-          <a href="#project" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Projects</a>
-          <a href="#contact" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Contact</a>
+        <div className="hidden space-x-6 md:flex">
+          <a href="#home"className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Home</a>
+          <a href="#about" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">About</a>
+          <a href="#service" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Services</a>
+          <a href="#project" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Projects</a>
+          <a href="#contact" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Contact</a>
         </div>
 
         {/* Desktop Button */}
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full">
+        <button className="hidden px-4 py-2 text-white transition-transform duration-300 transform rounded-full bg-gradient-to-r from-green-400 to-blue-500 md:inline hover:scale-105">
           Contact Me
         </button>
       </div>
@@ -51,12 +51,12 @@ const Navbar = () => {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="md:hidden">
-          <div className="flex flex-col space-y-4 mt-4 text-center">
-            <a href="#home" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Home</a>
-            <a href="#about" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">About Me</a>
-            <a href="#service" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Services</a>
-            <a href="#project" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Projects</a>
-            <a href="#contact" className="p-1 hover:text-white font-bold hover:bg-gradient-to-r from-green-300 to-blue-500 rounded">Contact</a>
+          <div className="flex flex-col mt-4 space-y-4 text-center">
+            <a href="#home" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Home</a>
+            <a href="#about" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">About Me</a>
+            <a href="#service" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Services</a>
+            <a href="#project" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Projects</a>
+            <a href="#contact" className="p-1 font-bold rounded hover:text-white hover:bg-gradient-to-r from-green-300 to-blue-500">Contact</a>
           </div>
         </div>
       )}
