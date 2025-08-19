8d4c1ea471c3430d1cf6c789e886856f29767f43
dankukennedy <dankukennedy@gmail.com>
2025-08-19 07:55:51 +0000

    vim

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,25 @@ const Footer = () => {
 
     //API  https://portfolioapi-ixx3.onrender.com
   return (
-    <footer className='bg-black text-white py-8'>
-        <div className='container  mx-auto px-8 md:px-16 lg:px-24' >
-            <div className='flex flex-col md:flex-row md:space-x-12 items-center mb-4' >
+    <footer className='py-8 text-white bg-black'>
+        <div className='container px-8 mx-auto md:px-16 lg:px-24' >
+            <div className='flex flex-col items-center mb-4 md:flex-row md:space-x-12' >
                 <div className='flex-1 mb-4 md:mb-0'>
-                <h3 className='text-2xl font-bold mb-2'>Danku Kennedy Edem</h3>
+                <h3 className='mb-2 text-2xl font-bold'>Danku Kennedy Edem</h3>
                 <p className='text-gray-400'>
                    Software Engineer based in Accra Ghana, specializing in web and software development
                 </p>
                </div>
              <div className='flex-1 w-full'>
                 <form action="" className='flex items-center justify-center'>
-                    <input type="email" placeholder='Enter Email' className='w-full p-2 rounded-l-lg bg-gray-800 border-gray-600 focus:outline-none focus:border-green-400'/>
+                    <input type="email" placeholder='Enter Email' className='w-full p-2 bg-gray-800 border-gray-600 rounded-l-lg focus:outline-none focus:border-green-400'/>
                     <button type='submit'
-                    className='bg-gradient-to-r from-green-400 to-blue-500 text-white px-4 py-2 rounded-r-lg'>Subscribe</button>
+                    className='px-4 py-2 text-white rounded-r-lg bg-gradient-to-r from-green-400 to-blue-500'>Subscribe</button>
                 </form>
              </div>
             </div>
 
-            <div className='border-t border-gray-600 pt-4 flex flex-col md:flex-row justify-between items-center'>
+            <div className='flex flex-col items-center justify-between pt-4 border-t border-gray-600 md:flex-row'>
                 <p className='text-gray-400'>
                   Copyright  &copy; {new Date().getFullYear()} Danku Kennedy Edem. All right reserved.
                 </p>
@@ -44,10 +44,10 @@ const Footer = () => {
                         <FaTelegram/>
                      </a>
                      <div className='flex space-x-4 md:my-0'>
-                        <a href="" className='text-gray-400 hover:text-white'>
+                        <a href="#" className='text-gray-400 hover:text-white'>
                             Privacy
                         </a>
-                        <a href="" className='text-gray-400 hover:text-white'>
+                        <a href="#" className='text-gray-400 hover:text-white'>
                             Terms of Service
                         </a>
 
