fd8b44741e1b915c7791b5339d297d8f8da9dcae
dankukennedy <dankukennedy@users.noreply.github.com>
2026-06-24 03:36:39 +0000

    vim

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/portfolio/', // Adjust based on your deployment
+  base: '/portfolio', // Adjust based on your deployment
   build: {
     rollupOptions: {
       input: './index.html', // Explicitly set the entry point
