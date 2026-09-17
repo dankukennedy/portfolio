9f3a2cd82f203d3f0af7d35186a26c46517a34e6
Danku kennedy Edem <dankukennedy@gmail.com>
2026-09-17 03:11:28 +0000

    vim

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: '/portfolio', // Adjust based on your deployment
+  base: '/portfolio/', // Adjust based on your deployment
   optimizeDeps: {
     include: ['react', 'react-dom'], // Add dependencies for pre-bundling
   },
